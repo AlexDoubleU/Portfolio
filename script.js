66d6d29b60ecc64e035786972cd3bff6715385de
AlexDoubleU <alexgwilson@hotmail.com>
2020-11-26 20:14:20 +0000

    Added slideshow js and closing menus

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
 let page = document.getElementById("fadein");
 function fadeIn(){
-page.classList.toggle('fade');
+page.classList.toggle('faded');
 }
 
 window.onload = fadeIn;
+
+function closeMenu() {
+  document.getElementById("showblock").checked = false;
+}
+
+var slideIndex = 1;
+
+function showSlide(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+}
+slides[slideIndex-1].style.display = "block";
+dots[slideIndex-1].className += " active";
+} 
+
+function plusSlides(n) {
+  showSlide(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlide(slideIndex = n);
+}
+
+showSlide(slideIndex);
