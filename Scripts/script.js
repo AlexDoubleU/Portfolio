e4adc773c69caeed6b1573b23c7926d135e24f0c
AlexDoubleU <alexgwilson@hotmail.com>
2020-12-12 01:44:04 +0000

    Added comments for increased clarification
    Also edited the about me description on index

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,3 +1,4 @@
+// sets the showblock checkbox to unchecked when called
 function closeMenu() {
   document.getElementById("showblock").checked = false;
 }
@@ -8,18 +9,25 @@ function showSlide(n) {
   var i;
   var slides = document.getElementsByClassName("slide-container");
   var dots = document.getElementsByClassName("dot");
+  // if the slide to switch to is higher than the number of slides go to first slide
   if (n > slides.length) {slideIndex = 1}
+  // if the slide to switch to is less than 1 go to the last slide
   if (n < 1) {slideIndex = slides.length}
+  // sets all slides to hidden
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none"; 
   }
+  // removes class active from dots
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
 }
+
+// sets the slide to show as block and the respective dot with class active
 slides[slideIndex-1].style.display = "block";
 dots[slideIndex-1].className += " active";
 } 
 
+// move from the current slide n positions
 function plusSlides(n) {
   showSlide(slideIndex += n);
 }
